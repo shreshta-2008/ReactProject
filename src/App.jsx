526e27ba63dab23a4766756985b05b5754d39bf5
shreshta-2008 <shreshtapapineni@gmail.com>
2026-01-08 08:31:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Student Information</h1>
+      <p>Name: Nyra </p>
+      <p>Roll No: 101</p>
+      <p>Department: CSE</p>
+      <p>Year: 2nd</p>
+      <p>Name: Ansh Rathod</p>
+      <p>Roll No: 102</p>
+      <p>Department:ECE </p>
+      <p>Year :1st</p>
+      </div>
+  );
+}
+
+export default StudentInfo;
+
+  
